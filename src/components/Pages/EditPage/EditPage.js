5b7b74e44c469c6233986b2d0233f80d203553fb
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-25 16:55:51 -0500

    added Cancel and Submit buttons to EditPage. Code for cancelClick button

--- src/components/Pages/EditPage/EditPage.js
+++ src/components/Pages/EditPage/EditPage.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class EditPage extends Component {
+  cancelClick = (event) => {
+    event.preventDefault();
+    this.props.history.push("/details");
+  };
+
   render() {
     return (
       <div>
         <h1>Edit</h1>
+        <button onClick={this.cancelClick}>Cancel</button>
+        <button>Save</button>
       </div>
     );
   }
