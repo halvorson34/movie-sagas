b0ef4dc829b3a302c0d9cf9487762ab0ac867dc9
Matt Halvorson <halvorson34.mh@gmail.com>
2020-04-26 04:02:57 -0500

    added input box and text area box with labels and buttons to edit page

--- src/components/Pages/EditPage/EditPage.js
+++ src/components/Pages/EditPage/EditPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./EditPage.css";
 
 class EditPage extends Component {
   cancelClick = (event) => {
@@ -11,8 +12,24 @@ class EditPage extends Component {
     return (
       <div>
         <h1>Edit</h1>
-        <button onClick={this.cancelClick}>Cancel</button>
-        <button>Save</button>
+        <form class="inputField">
+          <div class="titleInput">
+            <label for="inputLabel">Change Title</label>
+            <br />
+            <input placeholder="change movie title" class="inputLabel" />
+          </div>
+          <div class="textAreaInput">
+            <label for="textAreaLabel">Change Description</label>
+            <br />
+            <textarea rows="5" cols="50" class="textAreaLabel"></textarea>
+          </div>
+          <div>
+            <button onClick={this.cancelClick}>Cancel</button>
+          </div>
+          <div>
+            <button>Save</button>
+          </div>
+        </form>
       </div>
     );
   }
